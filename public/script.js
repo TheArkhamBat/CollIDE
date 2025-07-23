09e1e8338ac6d7528b9808e54ff5911377f34f55
Rohit Tomar <rohittomar788@Gmail.com>
2025-07-24 00:05:44 +0530

    Socket.io initialised

--- public/script.js
+++ public/script.js
@@ -1,1 +1,10 @@
+const socket=io();//calls socket.io client and connects to the server
 console.log("script.js loaded successfully!");
+
+socket.on('connect',()=>{
+    console.log("Connected to Socket.io server");
+});
+
+socket.on('disconnect',()=>{
+    console.log("Disconnected from Socket.io server");
+});
--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express=require('express');//import express
 const app=express();//create an instance of express
 const PORT=3000;//port we are using
+const http=require('http');//import http; required to bind socket.io
+const {Server}= require('socket.io');//extract server class from socket.io (destructuring)
+
+const server=http.createServer(app);//create a raw http server; passes to app to respond whenever there is any incoming request
+const io=new Server(server);//bind socket.io to the server
+
 app.use(express.static('public'));//this sets up a static server from 'public' folder
+
+io.on('connection',(socket)=>{//triggered when client connects
+    console.log("A user connected.");
+
+
+    socket.on('disconnect',()=>{//triggered when client disconnects
+        console.log("A user has disconnected.");
+    });
+});
 //Console log whenever express acts as a static server
-app.listen(PORT,()=>{
+server.listen(PORT,()=>{
     console.log(`Server is running at http:/localhost:${PORT}`)
 })
