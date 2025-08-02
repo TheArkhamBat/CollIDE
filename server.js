98855b268fd13efbcffe82747435b967d1ad8a3d
TheArkhamBat <steamaditya50@gmail.com>
2025-08-02 22:32:26 +0530

    Private room functionality added brah

--- server.js
+++ server.js
@@ -12,51 +12,72 @@ const users = new Map();
 
 app.use(express.static('public'));//this sets up a static server from 'public' folder
 
-io.on('connection',(socket)=>{//triggered when client connects
-    console.log("A user connected.");
+//now supports everything along with private room 
 
-    //when code is changed, emit code-update event with new code
-    socket.on('code-change',(code)=>{
-        socket.broadcast.emit('code-update',code);
-    })
+io.on('connection', (socket) => {
+    console.log("A user connected:", socket.id);
 
-    //when scratchpad text is changed, emit scratchpad-update event with new text
-    socket.on('scratchpad-change',(scratchpadText)=>{
-        socket.broadcast.emit('scratchpad-update',scratchpadText);
-    })
+    // This listens to new users joining a specific room
+    socket.on('new-user-joined', ({ username, room }) => {
+        // Join the socket to the specified room
+        socket.join(room);
+        // Store user info, including the room
+        users.set(socket.id, { username, room });
+        console.log(`${username} joined room: ${room}`);
 
-    //this listens to new users joined event 
-    socket.on('new-user-joined', (username) => {
-        users.set(socket.id, username);
-        const userList = Array.from(users.values());
-        io.emit('update-user-list', userList);
+        // Get a list of users ONLY in the current room
+        const roomUsers = Array.from(users.values())
+            .filter(user => user.room === room)
+            .map(user => user.username);
+        
+        // Broadcast the updated user list to everyone in that room
+        io.to(room).emit('update-user-list', roomUsers);
     });
 
-    //broadcasts users disconnecting 
-    socket.on('disconnect', () => {
-        const username = users.get(socket.id);
-        if (username) {
-            console.log(`${username} has disconnected.`);
-            users.delete(socket.id);
-            const userList = Array.from(users.values());
-            io.emit('update-user-list', userList);
-        } else {
-            console.log("An anonymous user has disconnected.");
+    // When code is changed, broadcast ONLY to the same room
+    socket.on('code-change', (code) => {
+        const user = users.get(socket.id);
+        if (user) {
+            socket.to(user.room).emit('code-update', code);
         }
     });
 
-    // Listen for when a client is typing
+    // When scratchpad text is changed, broadcast ONLY to the same room
+    socket.on('scratchpad-change', (scratchpadText) => {
+        const user = users.get(socket.id);
+        if (user) {
+            socket.to(user.room).emit('scratchpad-update', scratchpadText);
+        }
+    });
+
+    // When a client is typing, broadcast ONLY to the same room
     socket.on('typing', () => {
-        // Get the username associated with this socket
-        const username = users.get(socket.id);
-        if (username) {
-            // Broadcast to all other clients that this user is typing
-            socket.broadcast.emit('user-typing', username);
+        const user = users.get(socket.id);
+        if (user) {
+            socket.to(user.room).emit('user-typing', user.username);
         }
     });
 
+    // When a user disconnects, update the user list for that room
+    socket.on('disconnect', () => {
+        const user = users.get(socket.id);
+        if (user) {
+            console.log(`${user.username} from room ${user.room} has disconnected.`);
+            // Remove the user from the map
+            users.delete(socket.id);
+
+            // Get the new list of users for that room
+            const roomUsers = Array.from(users.values())
+                .filter(u => u.room === user.room)
+                .map(u => u.username);
+
+            // Broadcast the updated list to the remaining clients in the room
+            io.to(user.room).emit('update-user-list', roomUsers);
+        }
+    });
 });
+
 //Console log whenever express acts as a static server
 server.listen(PORT,()=>{
-    console.log(`Server is running at http:/localhost:${PORT}`)
+    console.log(`Server is running at http://localhost:${PORT}`)
 })
